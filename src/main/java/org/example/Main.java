70582636b88b576f0e18dbe66e0fcf4e3a1941ed
wail-ait <wail.ait.hioua@gmail.com>
2025-12-20 15:32:32 +0100

    commit to test webhook --MAIN--

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,14 +4,13 @@ package org.example;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        Calculator calculator = new Calculator();
+        int sum = calculator.add(10, 5);
+        int difference = calculator.subtract(10, 5);
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Difference: " + difference);
+
     }
 }
